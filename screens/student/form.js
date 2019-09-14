e2e9d5a577c848eea45bff6b15ea04dcc52065c2
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2019-09-14 09:14:08 -0400

    master

--- screens/student/form.js
+++ screens/student/form.js
@@ -1,52 +1,48 @@
 import React from 'react';
 import RadioButton from '../components/RadioButton.js';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default class Student extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            items: [
-                {
-                    title: "a student",
-                    key: 0,
-                },
-                {
-                    title: "a staff or faculty member",
-                    key: 1,
-                },
-                {
-                    title: "someone else",
-                    key: 2,
-                }
-            ],
-            buttonSelected: "a student",
-        }
-    }
-    render(){
-        let buttonChanged = (title) => {
-            this.setState({buttonSelected: title})
-        }
-        return (
-            <View style={styles.wrapper}>
-                <RadioButton
-                    items={this.state.items}
-                    onChange={buttonChanged}
-                >
-                </RadioButton>
-                <Text>{this.state.buttonSelected}</Text>
-            </View>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          title: 'a student',
+          key: 0,
+        },
+        {
+          title: 'a staff or faculty member',
+          key: 1,
+        },
+        {
+          title: 'someone else',
+          key: 2,
+        },
+      ],
+      buttonSelected: 'a student',
+    };
+  }
+  render() {
+    let buttonChanged = title => {
+      this.setState({ buttonSelected: title });
+    };
+    return (
+      <View style={styles.wrapper}>
+        <RadioButton items={this.state.items} onChange={buttonChanged} />
+        <Text>{this.state.buttonSelected}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        width: "100%",
-        height: "100%",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignContent: "center",
-        padding: 20,
-    }
-})
+  wrapper: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 20,
+  },
+});
---
+++ screens/student/login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+export default class StudentLogin extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={styles.wrapperMain}>
+        <Text style={styles.title}>Freedom High School</Text>
+        <Image style={styles.imageMain} source={require('../../assets/kyle.png')} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapperMain: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  imageMain: {
+    width: 100,
+    height: 100,
+    margin: 'auto',
+  },
+  title: {
+    fontSize: 26,
+    alignSelf: 'flex-start',
+  },
+});
